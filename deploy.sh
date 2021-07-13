8aa3cbb4ac9fd513f897bc8ad84f3e5fd4a269d4
kotwys <52920928+kotwys@users.noreply.github.com>
2021-07-13 13:06:05 +0400

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [[ -n $(git status -s) ]]
+then
+  echo "Dirty git tree."
+  exit 1
+fi
+
+REPO=ssh://git@github.com/kotwys/kotwys.github.io.git
+commit=$(git rev-parse HEAD)
+
+yarn build
+cd ./build
+
+git init
+git add -A
+git commit -m "Deploy from @ $commit"
+
+git push -f $REPO main
+
+cd -
